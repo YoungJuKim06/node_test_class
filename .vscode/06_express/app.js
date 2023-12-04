925029b034a0632a47da37b961cd530427b93d1a
YoungJuKim06 <121931453+YoungJuKim06@users.noreply.github.com>
2023-12-04 17:58:55 +0900

    2023-12-04 express
    cookie,session,express 학습

---
+++ .vscode/06_express/app.js
@@ -0,0 +1,91 @@
+// ------------------------------ ver.1
+
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+// app.set('port', process.env.PORT || 3000);
+// // 서버 실행 포트 설정
+// // process.env에 port설정이 있다면 그 값을 쓰고, 없다면 기본 3000포트
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/index.html')); // res.write, end 대신 send 사용
+// });
+
+// app.listen(app.get('port'), () => {
+//     console.log(app.get('port'), '번 포트에서 대기 중');
+// });
+
+
+// ------------------------------ ver.2
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+// app.set('port', process.env.PORT || 3000);
+
+// app.use((req, res, next) => {
+//     console.log('모든 요청에 다 실행된다.');
+//     next();
+// });
+
+// app.use('/', (req, res, next) => {
+//     console.log('GET / 요청에서만 실행된다.');
+//     next();
+// }, (req, res) => {
+//     throw new Error('에러는 에러처리 미들웨어로 간다.')
+// });
+
+// app.use((err, req, res, next) => {
+//     console.log(err);
+//     res.status(500).send(err.message);
+// });
+
+
+// app.listen(app.get('port'), () => {
+//     console.log(app.get('port'), '번 포트에서 대기 중');
+// });
+
+
+// ------------------------------ ver.3
+
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const path = require('path');
+
+
+dotenv.config();
+const app = express();
+app.set('port', process.env.PORT || 3000);
+
+app.use(morgan('dev'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+    name: 'session-cookie',
+}));
+
+app.use((req, res, next) => {
+    console.log('모든 요청이 다 실행된다.');
+    next();
+});
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기 중');
+});
+
+
+
+
